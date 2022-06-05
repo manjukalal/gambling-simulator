6bd91bba80004cc80ed5440e995553a9babb9286
manjukalal <manjukalal1496@gmail.com>
2022-06-05 20:13:26 +0530

    Added:welcome message

---
+++ src/com/bridgelabz/GamblingSimulatorProgram.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class GamblingSimulatorProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Gambler Simulator ");
+
+
+    }
+}
