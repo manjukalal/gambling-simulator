017dd704e7e79c8f10a0e6954e3c938057896a3a
manjukalal <manjukalal1496@gmail.com>
2022-06-05 21:01:09 +0530

    Added:logic for finding luckiest or unluckiest day

--- src/com/bridgelabz/GamblingSimulatorProgram.java
+++ src/com/bridgelabz/GamblingSimulatorProgram.java
@@ -6,6 +6,7 @@ public class GamblingSimulatorProgram {
     final static int Bet_Every_Game = 1;
     static int Every_Day_Stack = 100;
     static int Loss = 0;
+    static int Win = 0;
     static int gain = 0;
     static int WinDays = 0;
     static int LossDays = 0;
@@ -22,15 +23,15 @@ public class GamblingSimulatorProgram {
             Every_Day_Stack = 100;
             while (Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
                 double random = Math.floor(Math.random() * 10) % 2;
-//
+
                 if (random == 1) {
-                    int win = 1;
-                    Every_Day_Stack = Every_Day_Stack + win;
+                    Win = 1;
+                    Every_Day_Stack = Every_Day_Stack + Win;
                     gain++;
 
                 } else {
-                    int loss = 1;
-                    Every_Day_Stack = Every_Day_Stack - loss;
+                    Loss = 1;
+                    Every_Day_Stack = Every_Day_Stack - Loss;
                     gain--;
                 }
             }
@@ -47,22 +48,25 @@ public class GamblingSimulatorProgram {
                 LossDays += 1;
                 lossing = 50;
                 System.out.println("lossing amt per day:" + lossing);
-
                 System.out.println("total amount remaining : " + Every_Day_Stack);
-
+            }
+            if (Every_Day_Stack > 50)
+            {
+                System.out.println("luckiest day:" +day);
+            } else if (Every_Day_Stack <= 50) {
+                System.out.println("unluckiest day:" +day);
 
             }
 
             System.out.println(day);
-
-
         }
+
 
         System.out.println("total win days" + WinDays);
         System.out.println("total loss days" + LossDays);
 
         System.out.println("Total Win Days: " + WinDays + "total amount win days amount: " + Winning * WinDays);
         System.out.println("Total Loss Days: " + LossDays + "total amount loss day amount: " + lossing * LossDays);
+
     }
 }
-
