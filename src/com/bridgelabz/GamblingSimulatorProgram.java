1d256b4989fe3a08f0546c7d4f6dc0c7f430ba90
manjukalal <manjukalal1496@gmail.com>
2022-06-05 20:55:46 +0530

    Added:logic calculating total amount for month

--- src/com/bridgelabz/GamblingSimulatorProgram.java
+++ src/com/bridgelabz/GamblingSimulatorProgram.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 import java.util.*;
 
 public class GamblingSimulatorProgram {
-
     final static int Bet_Every_Game = 1;
     static int Every_Day_Stack = 100;
     static int Loss = 0;
@@ -19,10 +18,11 @@ public class GamblingSimulatorProgram {
 
 
         System.out.println("Welcome To Gambler Simulator");
-        for (int day = 1; day <= 20; day++) {
+        for (int day = 1; day <= 30; day++) {
             Every_Day_Stack = 100;
             while (Every_Day_Stack > Stack_Lower && Every_Day_Stack < Stack_Upper) {
                 double random = Math.floor(Math.random() * 10) % 2;
+//
                 if (random == 1) {
                     int win = 1;
                     Every_Day_Stack = Every_Day_Stack + win;
@@ -58,10 +58,11 @@ public class GamblingSimulatorProgram {
 
         }
 
+        System.out.println("total win days" + WinDays);
+        System.out.println("total loss days" + LossDays);
+
+        System.out.println("Total Win Days: " + WinDays + "total amount win days amount: " + Winning * WinDays);
+        System.out.println("Total Loss Days: " + LossDays + "total amount loss day amount: " + lossing * LossDays);
     }
 }
-
-
-
-
 
