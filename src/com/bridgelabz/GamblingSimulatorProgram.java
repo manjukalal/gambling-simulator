36c29414f2d65419035e71d3815d2a641300dea9
manjukalal <manjukalal1496@gmail.com>
2022-06-05 20:22:12 +0530

    Added:dollars for initial condition

--- src/com/bridgelabz/GamblingSimulatorProgram.java
+++ src/com/bridgelabz/GamblingSimulatorProgram.java
@@ -1,6 +1,9 @@
 package com.bridgelabz;
+import java.util.*;
 
 public class GamblingSimulatorProgram {
+    final static int Bet_Every_Game = 1;
+    static int Every_Day_Stack = 100;
     public static void main(String[] args) {
 
         System.out.println("Welcome To Gambler Simulator ");
